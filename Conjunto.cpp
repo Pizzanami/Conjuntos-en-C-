cf0456b30a716fdfee046fbb562122dfb073c025
[Pizzanami] <[itzanami.berlanga@gmail.com]>
2020-02-04 17:39:08 -0600

    funciones y cosas x2

--- Conjunto.cpp
+++ Conjunto.cpp
@@ -1,6 +1,11 @@
 #include "Conjunto.h"
 Conjunto::Conjunto(){}
-Conjunto::int esta(int e){
+void Conjunto::intercambiar(int x, int y){
+    c[x] = c[x] + c[y];
+    c[y] = c[x] - c[y];
+    c[x] = c[x] - c[y];
+}
+int Conjunto::esta(int e){
     for(int i = 0: i < c.size(); i++){
         if(c[i] == e)
             return i;
@@ -8,20 +13,25 @@ Conjunto::int esta(int e){
     return -1;
 }
 Conjunto::friend void operator +(Conjunto const &a, int e){
-    if(a.esta(e) == -1){
-        a.c.push_back(e);
+    for(int i = 0: i < c.size(); i++){
+        if(c[i] == e) return;
+        if(c[i] > e){
+            c.insert(c.begin() + i-1, e);
+        }
     }
 }
-Conjunto::friend void operator +(Conjunto const &a, Conjunto const &b){
+Conjunto::friend  operator +(Conjunto const a, Conjunto const b){
+    Conjunto nuevo = a;
     for(int i = 0; i < b.c.size(); i++){
-        a = a + b.c[i];
+        nuevo = nuevo + b.c[i];
     }
+    return nuevo;
 }
 Conjunto::friend bool operator ==(Conjunto const &a, Conjunto const &b){
     if(a.c.size != b.c.size())
         return 0;
     for(int i = 0; i < b.c.size(); i++)
-        if(a.esta(b[i]) == -1) return 0;
+        if(a.c[i] != b.c[i]) return 0;
     return 1;
 }
 Conjunto::friend void operator -(Conjunto const &a, Conjunto const &b){
--- Conjunto.h
+++ Conjunto.h
@@ -2,12 +2,14 @@
 class Conjunto{
     private:
         vector <int>c;
+        void intercanbiar(int x, int y);
     public:
         Conjunto();
         int esta(int e);
-        friend void operator +(Conjunto const &a, int e);
+        friend void operator +(Conjunto const &a, int e);//Agregar elemento
         friend void operator +(Conjunto const &a, Conjunto const &b);//Union de conjuntos
         friend void operator ==(Conjunto const &a, Conjunto const &b);//comparaciones
+        friend void operator -(Conjunto const &a, int e);//Eliminar un elemento
         friend void operator -(Conjunto const &a, Conjunto const &b);//Restar
         friend void operator /(Conjunto const &a, Conjunto const &b);//Interseccion
         friend void operator %(Conjunto const &a, Conjunto const &b);//Union - Interseccion
